459ecc85aa48dbba0ba247ff5c1898658b1aadf8
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-04-19 03:05:55 +0900

    Create MidTerm01.java

---
+++ StudyingForExams/Midterm23/MidTerm01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Midterm01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("width of square : ");
+        int a = sc.nextInt();
+
+        System.out.print("height of square : ");
+        int b = sc.nextInt();
+
+        double s = a*b;
+        double w = (2 * a) + (2 * b);
+
+        System.out.println("Area of square : " + s);
+        System.out.println("Length of square : " + w);
+    }
+}
