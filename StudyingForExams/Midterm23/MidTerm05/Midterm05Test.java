d6a31e92dcf03d7a7bb4cb5bf422d2c4e74f9edc
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-04-19 03:11:24 +0900

    Create Midterm05Test.java

---
+++ StudyingForExams/Midterm23/MidTerm05/Midterm05Test.java
@@ -0,0 +1,9 @@
+public class Midterm05Test {
+    public static void main(String[] args) {
+        Midterm05 obj[] = new Midterm05[2];
+        obj[0] = new dog();
+        obj[1] = new cat();
+        for(int i = 0; i<2; i++)
+            obj[i].sound();
+    }
+}
